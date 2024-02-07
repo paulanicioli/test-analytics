e414140f6ad8cb212d46ab4be1b1c137fd800a3d
Paula Nicioli <76572461+paulanicioli@users.noreply.github.com>
2024-02-06 22:14:48 -0800

    Update cookie-banner.js

--- src/cookie-banner.js
+++ src/cookie-banner.js
@@ -16,6 +16,7 @@ function showCookieBanner(){
 function hideCookieBanner(){
     localStorage.setItem("cb_isCookieAccepted", "yes");
     document.cookie = "analytics_storage=true";
+    document.cookie = "ads_storage=true";
 
     let cookieBanner = document.getElementById("cb-cookie-banner");
     cookieBanner.style.display = "none";
