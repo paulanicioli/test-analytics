4dc37456dc7170245f2f02374cd8a7bb3e0a85e2
Paula Nicioli <paulanicioli@gmail.com>
2024-02-12 22:48:45 -0800

    loads error message toast

--- src/listeners.js
+++ src/listeners.js
@@ -6,3 +6,13 @@ function leadForm() {
   dataLayer.push({'email': getElementById("email").value});
   console.log('Form submitted!');
 }
+
+// Button clicks listeners
+
+function buttonClickEvent (event) {
+  dataLayer.push({'event': event.id});
+  document.getElementById('errorMessage').show();
+}
+
+
+window.bn_buttonClickEvent = buttonClickEvent;
