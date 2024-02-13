42817f3acbf437827dc4c6c56ab921463d0f1ced
Paula Nicioli <paulanicioli@gmail.com>
2024-02-12 23:14:42 -0800

    fixes toast implementation

--- src/listeners.js
+++ src/listeners.js
@@ -13,10 +13,9 @@ function buttonClickEvent (event) {
   console.log("buttonClickEvent called!");
   console.log("event_id:",event.id);
   dataLayer.push({'event': event.id});
-  var myToastEl = document.getElementById('errorMessage');
-  myToastEl.addEventListener('hidden.bs.toast', function () {
-  // do something...
-})
+
+  const myToast = new bootstrap.Toast(document.getElementById('errorMessage'));
+  myToast.show();
 
 }
 
